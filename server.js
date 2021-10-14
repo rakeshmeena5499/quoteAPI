da405c4b2c05019c7c5883a3978ccaf981e5fd76
pavlo-shevchuk <pavlo.shevchuk@sombrainc.com>
2021-10-14 14:22:29 +0300

    little refactoring

--- server.js
+++ server.js
@@ -8,42 +8,47 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-app.get('/api/quotes/random', (req, res, next)=>{
-	res.send({
-		quote: getRandomElement(quotes)
-	});
+app.get('/api/quotes/random', (req, res) => {
+  res.send({
+    quote: getRandomElement(quotes)
+  });
 });
 
-app.get('/api/quotes', (req, res, next)=>{
-	if(req.query.person !== undefined){
-		const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
-		res.send({
-			quotes: quotesByPerson
-		});
-	}
-	else{
-		res.send({
-			quotes:quotes
-		});
-	}
+app.get('/api/quotes', (req, res) => {
+  if (req.query.person) {
+    res.send({
+      quotes: quotes.filter(quote => quote.person === req.query.person)
+    });
+  } else {
+    res.send({
+      quotes: quotes
+    });
+  }
 });
 
-app.post('/api/quotes', (req, res, next)=>{
-	const newQuote = {
-		quote: req.query.quote,
-		person: req.query.person
-	};
-	if(newQuote.quote && newQuote.person){
-		quotes.push(newQuote);
-		res.send({
-			quote: newQuote
-		});
-	}
-	else{
-		res.status(400).send();
-	}
+app.post('/api/quotes', (req, res) => {
+  const newQuote = {
+    quote: req.query.quote,
+    person: req.query.person
+  };
+  if (newQuote.quote && newQuote.person) {
+    quotes.push(newQuote);
+    res.send({
+      quote: newQuote
+    });
+  } else {
+    res.status(400).send();
+  }
 });
 
-app.listen(PORT, ()=>{
-	console.log('Server listen at '+PORT);
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+
+  console.log(`Server listen at ${PORT}`);
+});
+
+// promise error
+process.on('unhandledRejection', reason => {
+  console.log(reason);
+  process.exit(0);
 });
